fe818e74ead84ac63a4d6c5f3201fd570d3077de
popovas <popovas@209-9>
2025-10-07 18:36:35 +0200

    sad

--- lab3api/Model/SeedData.cs
+++ lab3api/Model/SeedData.cs
@@ -13,13 +13,13 @@ namespace lab3api.Model
               
                 var purchases = new List<Purchase>
                 {
-                    new Purchase { Article = "1", Name = "Кроссовки", UnitPrice = 100 },
-                    new Purchase { Article = "2", Name = "Ботинки", UnitPrice = 150 },
-                    new Purchase { Article = "3", Name = "Сандалии", UnitPrice = 80 },
-                    new Purchase { Article = "4", Name = "Туфли", UnitPrice = 120 },
-                    new Purchase { Article = "5", Name = "Сапоги", UnitPrice = 200 },
-                    new Purchase { Article = "6", Name = "Кеды", UnitPrice = 90 },
-                    new Purchase { Article = "7", Name = "Тапочки", UnitPrice = 50 }
+                    new Purchase { Article = "a1", Name = "Кроссовки", UnitPrice = 100 },
+                    new Purchase { Article = "a2", Name = "Ботинки", UnitPrice = 150 },
+                    new Purchase { Article = "a3", Name = "Сандалии", UnitPrice = 80 },
+                    new Purchase { Article = "a4", Name = "Туфли", UnitPrice = 120 },
+                    new Purchase { Article = "a5", Name = "Сапоги", UnitPrice = 200 },
+                    new Purchase { Article = "a6", Name = "Кеды", UnitPrice = 90 },
+                    new Purchase { Article = "a7", Name = "Тапочки", UnitPrice = 50 }
                 };
 
                 context.Purchases.AddRange(purchases);
@@ -27,13 +27,13 @@ namespace lab3api.Model
                 
                 var sales = new List<Sale>
                 {
-                    new Sale { SaleDate = new DateTime(2025,10,1), Article = "1", Name = "Кроссовки", Quantity = 2, TotalPrice = 200 },
-                    new Sale { SaleDate = new DateTime(2025,10,1), Article = "2", Name = "Ботинки", Quantity = 1, TotalPrice = 150 },
-                    new Sale { SaleDate = new DateTime(2025,10,2), Article = "1", Name = "Кроссовки", Quantity = 1, TotalPrice = 100 },
-                    new Sale { SaleDate = new DateTime(2025,10,2), Article = "4", Name = "Туфли", Quantity = 3, TotalPrice = 360 },
-                    new Sale { SaleDate = new DateTime(2025,10,3), Article = "6", Name = "Кеды", Quantity = 5, TotalPrice = 450 },
-                    new Sale { SaleDate = new DateTime(2025,10,3), Article = "5", Name = "Сапоги", Quantity = 2, TotalPrice = 400 },
-                    new Sale { SaleDate = new DateTime(2025,10,4), Article = "3", Name = "Сандалии", Quantity = 4, TotalPrice = 320 }
+                    new Sale { SaleDate = new DateTime(2025,10,1), Article = "a1", Name = "Кроссовки", Quantity = 2, TotalPrice = 200 },
+                    new Sale { SaleDate = new DateTime(2025,10,1), Article = "a2", Name = "Ботинки", Quantity = 1, TotalPrice = 150 },
+                    new Sale { SaleDate = new DateTime(2025,10,2), Article = "a1", Name = "Кроссовки", Quantity = 1, TotalPrice = 100 },
+                    new Sale { SaleDate = new DateTime(2025,10,2), Article = "a4", Name = "Туфли", Quantity = 3, TotalPrice = 360 },
+                    new Sale { SaleDate = new DateTime(2025,10,3), Article = "a6", Name = "Кеды", Quantity = 5, TotalPrice = 450 },
+                    new Sale { SaleDate = new DateTime(2025,10,3), Article = "a5", Name = "Сапоги", Quantity = 2, TotalPrice = 400 },
+                    new Sale { SaleDate = new DateTime(2025,10,4), Article = "a3", Name = "Сандалии", Quantity = 4, TotalPrice = 320 }
                 };
 
                 context.Sales.AddRange(sales);
--- lab3api/Program.cs
+++ lab3api/Program.cs
@@ -42,11 +42,3 @@ var context = app.Services.CreateScope().ServiceProvider.
 SeedData.SeedDatabase(context);
 app.Run();
 
-public class AuthOptions
-{
-    public const string ISSUER = "MyAuthServer";
-    public const string AUDIENCE = "MyAuthClient";
-    const string KEY = "mysupersecret_secretsecretkey!123";
-    public static SymmetricSecurityKey GetSimmetricSecurutyKey() =>
-        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
-}
